12705559356043512ac461d0070807c5562591db
joe <jjhowe@protonmail.ch>
2024-11-26 00:40:49 +0000

    Fixed error with html mismatch? 😅

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,19 +1,33 @@
+import Navbar from '@/components/interface/navbar';
 import type {Metadata} from 'next';
+import localFont from "next/font/local";
+import "@/app/globals.css";
 
+const geistSans = localFont({
+  src: "../fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "../fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 const metadata: Metadata = {
   title: 'Task Tracker',
   description: 'Track your tasks'
 };
 
-export default function Layout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return(
-    <html lang='en'>
+    <html className='dark' lang='en'>
       <body>
+        <Navbar />
         {children}
       </body>
     </html>
