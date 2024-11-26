85b856722b0b38fae7780d97c709933787490e22
joe <jjhowe@protonmail.ch>
2024-11-26 21:04:54 +0000

    Created TaskList.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
 import TaskIndicator from "@/components/interface/TaskIndicator";
+import TaskList from "@/components/TaskList";
 import Image from "next/image";
 
 export default async function Home() {
   return (
     <div className="p-6 mx-auto lg:px-6 lg:py-3">
       <TaskIndicator />
+      <TaskList />
     </div>
   )
 }
