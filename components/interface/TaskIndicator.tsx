428630ddee859ddd84443f09a4243f3cc3878da9
joe <jjhowe@protonmail.ch>
2024-11-26 21:03:44 +0000

    Refactored TaskIndicator.

--- components/interface/TaskIndicator.tsx
+++ components/interface/TaskIndicator.tsx
@@ -1,28 +1,49 @@
 import React from 'react'
+import { UserTasksCategories } from '@/lib/types';
+import { FaCalendar, FaCheck, FaClock, FaExclamation } from 'react-icons/fa6';
+
+const Items: Array<UserTasksCategories> = [
+    {
+      name: 'Upcoming Tasks',
+      value: 5,
+      color: 'blue',
+      icon: <FaCalendar className="my-auto" />
+    },
+    {
+      name: 'Overdue Tasks',
+      value: 2,
+      color: 'red',
+      icon: <FaClock className="my-auto" />
+    },
+    {
+      name: 'Active Tasks',
+      value: 10,
+      color: 'green',
+      icon: <FaCheck className="my-auto" />
+    },
+    {
+      name: 'Important Tasks',
+      value: 3,
+      color: 'yellow',
+      icon: <FaExclamation className="my-auto" />
+    },
+];
 
 const TaskIndicator = () => {
+  
   return (
-    <section className="mx-auto grid gap-5 grid-flow-row scroll-smooth max-h-[200px] overflow-scroll md:max-h-full  md:grid-cols-4 lg:container">
-      <div className="p-6 rounded-lg dark:border dark:bg-neutral-950 bg-neutral-100 shadow-md">
-        <h1 className="text-3xl">Upcoming Tasks</h1>
-        <h2 className="text-2xl text-end my-3">0</h2>
-        <div className="border border-blue-400 mt-auto"></div>
-      </div>
-      <div className="p-6 rounded-lg dark:border dark:bg-neutral-950 bg-neutral-100 shadow-md">
-        <h1 className="text-3xl">Overdue Tasks</h1>
-        <h2 className="text-2xl text-end my-3">0</h2>
-        <div className="border border-red-400 mt-auto"></div>
-      </div>
-      <div className="p-6 rounded-lg dark:border dark:bg-neutral-950 bg-neutral-100 shadow-md">
-        <h1 className="text-3xl">Active Tasks</h1>
-        <h2 className="text-2xl text-end my-3">0</h2>
-        <div className="border border-green-400 mt-auto"></div>
-      </div>
-      <div className="p-6 rounded-lg dark:border dark:bg-neutral-950 bg-neutral-100 shadow-md">
-        <h1 className="text-3xl">Important Tasks</h1>
-        <h2 className="text-2xl text-end my-3">0</h2>
-        <div className="border border-yellow-400 mt-auto"></div>
-      </div>
+    <section className="mx-auto grid gap-5 grid-flow-row scroll-smooth max-h-[200px] md:overflow-visible overflow-y-scroll md:max-h-full md:grid-cols-4 lg:container">
+      
+      {Items?.length > 0 && (
+        Items.map((item: UserTasksCategories) => (
+          <div key={item?.name} className="p-6 rounded-lg dark:border dark:bg-neutral-900 bg-neutral-200 shadow-md md:hover:scale-110 cursor-pointer">
+            <h1 className="text-3xl">{item?.name}</h1>
+            <h2 className="text-2xl my-3 flex gap-3 justify-end">{item?.icon} {item?.value}</h2>
+            <div className={`border border-${item?.color}-400 mt-auto`}></div>
+          </div>
+        ))
+      )}
+      
     </section>
   )
 }
